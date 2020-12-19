97c6fedaeae2e7c0a511b953b7d587357030320e
administrator <administrator@administrator-Inspiron-15-3552>
2020-12-19 04:27:34 -0800

    dragon ball

--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -12,18 +12,17 @@ public class DragonFight {
 
 		ImageIcon dragon = new ImageIcon("src/_04_amazing_games/_2_dragon_fight/dragonPicture.jpg");
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
-
 		// 1. Create some variables to hold health levels
-		
+		int playerHealth=100;
 			// playerHealth to store your health - set it equal to 100
 	
 			// dragonHealth to store the dragon's health - set it equal to 100
-
+		int dragonHealth=100;
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
-		
+		int playerAttack=0;
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
-	
+			int dragonAttack=0;
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
 
@@ -31,22 +30,29 @@ public class DragonFight {
 		//  This while statement will cause the game attack code to repeat
 		while (true) {
 
+
 			// THE PLAYER ATTACKS THE DRAGON
 
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
-	
+				String input=JOptionPane.showInputDialog("yell or kick");
 				// 4. If they typed in "yell":
-	
+				if(input.equals("yell")) {
+					playerAttack=ran.nextInt(10);
+					
+				}
 					  // -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					  // ran.nextInt(10)
-			
+			if(input.equals("kick")); {
+				
+				playerAttack=ran.nextInt(25);
+			}
 				// 5. If they typed in "kick":
 	
 					  // -- Find a random number between 0 and 25 and store it in playerAttack.
 		
 				// 6. Subtract the player attack value from the dragon's health
-
+				dragonHealth-=playerAttack;
 			// THE DRAGON RETALIATES
 
 				// 7. Find a random number between 0 and 35 and store it in dragonAttack
